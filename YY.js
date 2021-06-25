9d00b5a056f1b6394f8ee181c59921bf415c3b26
MariannaTurch <mnturchaninova@miem.hse.ru>
2021-06-25 13:39:13 +0300

    Update YY.js

--- YY.js
+++ YY.js
@@ -1,46 +1,27 @@
 let ar = [3, 45, 35, 1, 789]
 function first() {
-   ar = ar.map(
-       function (n) {
-        return n * 2;
-    });
-   return ar;
+    return ar = ar.map (n => n * 2)
 }
 console.log(first());
 
 
 let ar2 = [3.3, 45, 35.42, 1.003, 789.09];
 function second() {
-    ar2 = ar2.map(
-        function (n) {
-            return Math.trunc(n);
-        });
-    return ar2;
+    return ar2 = ar2.map(n => Math.trunc(n))
 }
 console.log(second());
 
 
 let ar3 = [33, 45, 35.42, 3, 9];
 function third() {
-    ar3 = ar3.reduce(
-        function (sum,current) {
-            return sum+current;
-        },0);
-    return ar3;
+    return ar3 = ar3.reduce((sum,current) => sum+current,0)
 }
 console.log(third());
 
 
 let ar4 = [33, "kek","lol", 9];
 function fourth() {
-    ar4 = ar4.filter(
-        function (num) {
-            return typeof num === "number";
-        });
-    const sum = ar4.reduce(
-        function (sum4,current4) {
-            return sum4+current4;
-        },0);
-      return [ar4,sum];
+    ar4 = ar4.filter( num => typeof num === "number");
+    return sum = ar4.reduce((sum4,current4) => sum4+current4,0)
 }
 console.log(fourth());
